135e4965ab1537aeb3b9f7ab4a8db13460c7397a
Alex <ivanov.alexey.92@gmail.com>
2017-01-17 00:40:56 +0300

    greeting-user

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
 
+import readlineSync from 'readline-sync';
+
 'use strict';
 
-console.log('Welcome to the Brain Games!');
+console.log('Welcome to the Brain Games!\n');
+
+const userName = readlineSync.question('May I have your name? ');
+
+console.log(`Hello, ${userName}!`);
