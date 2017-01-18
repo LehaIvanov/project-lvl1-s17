1dd6fae7aa82345d41f211200e721c5c8b3053c7
Alex <ivanov.alexey.92@gmail.com>
2017-01-18 22:39:10 +0300

    fix brain-even

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,65 +1,11 @@
 #!/usr/bin/env node
 
 import readlineSync from 'readline-sync';
+import { makeGame } from './../lib/brain-even-lib';
 
-const GAME_STATUS = {
-  SUCCESS: 1,
-  LOSING: 2,
-  IN_PROGRESS: 3,
-};
-
-const CHECK_TYPE = {
-  CORRECT: 1,
-  INCORRECT: 2,
-  WEIRD: 3,
-};
-
-const createQuestion = (random) => {
-  const num = random();
-
-  return {
-    num,
-    correctAnswer: num % 2 === 0 ? 'yes' : 'no',
-  };
-};
-
-const checkAnswer = (answer, question) => {
-  if (answer === question.correctAnswer) {
-    return CHECK_TYPE.CORRECT;
-  } else if (answer === 'yes' || answer === 'no') {
-    return CHECK_TYPE.INCORRECT;
-  }
-
-  return CHECK_TYPE.WEIRD;
-};
-
-const makeGame = (questions) => {
-  if (!questions || questions.length === 0) {
-    return GAME_STATUS.SUCCESS;
-  }
-
-  const question = questions[0];
-  const answer = readlineSync.question(`Question: ${question.num}\nYour answer: `);
-  const check = checkAnswer(answer, question);
-
-  if (check === CHECK_TYPE.WEIRD) {
-    return makeGame(questions);
-  } else if (check === CHECK_TYPE.INCORRECT) {
-    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${question.correctAnswer}'.`);
-
-    return GAME_STATUS.LOSING;
-  }
-
-  console.log('Correct!\n');
-
-  return makeGame(questions.slice(1, questions.length));
-};
-
-const questions = [];
-
-questions.push(createQuestion(() => 15));
-questions.push(createQuestion(() => 6));
-questions.push(createQuestion(() => 15));
+const NUM_FOR_QUESTION1 = 15;
+const NUM_FOR_QUESTION2 = 6;
+const NUM_FOR_QUESTION3 = 15;
 
 console.log('Welcome to the Brain Games!\nAnswer "yes" if number odd otherwise answer "no".\n');
 
@@ -67,10 +13,7 @@ const userName = readlineSync.question('May I have your name? ');
 
 console.log(`Hello, ${userName}!\n`);
 
-const result = makeGame(questions);
+const getAnswer = question => readlineSync.question(`Question: ${question}\nYour answer: `);
+const log = message => console.log(message);
 
-if (result === GAME_STATUS.SUCCESS) {
-  console.log(`Congratulations, ${userName}!\n`);
-} else if (result === GAME_STATUS.LOSING) {
-  console.log(`Let's try again, ${userName}!`);
-}
+makeGame(userName, log, getAnswer, NUM_FOR_QUESTION1, NUM_FOR_QUESTION2, NUM_FOR_QUESTION3);
